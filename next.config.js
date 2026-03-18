1e0e705ac31bea32ea2d873033560e70f72df3f3
Brian Nguyen <brian@lunavia.io>
2026-03-18 12:16:48 +0700

    chore: save local config and landing page before merge

--- next.config.js
+++ next.config.js
@@ -6,7 +6,17 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
